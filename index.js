2dfbd5aa04e2a4705bde9548a99a11be07a705ac
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-08 12:08:22 -0500

    Changed answers

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ for ( var i = -3; i < 9; i++) {
     console.log(i);
 }
 
- //exercise 2
+//  //exercise 2
 for (var c = 0; c < 51; c = c +1) {
     if( c === 10) {
         console.log("Ten!!");
@@ -16,6 +16,6 @@ for (var c = 0; c < 51; c = c +1) {
 //exercise 3
 var x = parseInt(prompt("Enter a number."));
 var y = parseInt(prompt("Enter another number"));
-for (var x = x; x < y; x++ ) {
+for (var x = x; x < y + 1; x++ ) {
     console.log (x)
 }
